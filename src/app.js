1d7d0e5ea3613188473e9619426a88915b228811
Daniel Lanciana <daniel@ivy.com>
2018-07-02 19:15:29 -0400

    fix(test): start single process for tests, otherwise fail

--- src/app.js
+++ src/app.js
@@ -16,44 +16,55 @@ const ENVIRONMENT = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8080;
 const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-var app = function() {
-  return express()
-    // Security (https://expressjs.com/en/advanced/best-practice-security.html)
-    .use(helmet())
-    // View engine
-    .set('views', path.join(__dirname, '../views'))
-    .set('view engine', 'pug')
-    // Logging
-    .use(morgan('dev', {
-      skip: () => ENVIRONMENT === 'test'
-    }))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(express.static(path.join(__dirname, '../public')))
-    .use('/', routes)
-    // Catch 404 and forward to error handler
-    .use((req, res, next) => {
-      const err = new Error('Not Found');
-      err.status = 404;
-      next(err);
-    })
-    // Error handler
-    .use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-      res
-        .status(err.status || 500)
-        .render('error', {
-          message: err.message
-        });
-    })
-    .listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+const configExpress = function(app) {
+    app
+        // Security (https://expressjs.com/en/advanced/best-practice-security.html)
+        .use(helmet())
+        // View engine
+        .set('views', path.join(__dirname, '../views'))
+        .set('view engine', 'pug')
+        // Logging
+        .use(morgan('dev', {
+            skip: () => ENVIRONMENT === 'test'
+        }))
+        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(express.static(path.join(__dirname, '../public')))
+        .use('/', routes)
+        // Catch 404 and forward to error handler
+        .use((req, res, next) => {
+            const err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        })
+        // Error handler
+        .use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+            res
+                .status(err.status || 500)
+                .render('error', {
+                    message: err.message
+                });
+        })
+        .listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+
+    return app;
 };
 
-// Clustering concurrency
-// https://devcenter.heroku.com/articles/node-concurrency
-// https://github.com/hunterloftis/throng
-throng({
-  workers: WORKERS,
-  start: app
-});
+const startSingleProcess = function() {
+    const app = express();
+    return configExpress(app);
+};
+
+const startCluster = function() {
+    // Clustering concurrency
+    // https://devcenter.heroku.com/articles/node-concurrency
+    // https://github.com/hunterloftis/throng
+    throng({
+        workers: WORKERS,
+        start: startSingleProcess
+    });
+};
+
+const app = (ENVIRONMENT === 'test') ? startSingleProcess() : startCluster();
 
 export default app;
